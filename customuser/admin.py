e3921e2c242cfcd45e788ed9d21146130e9aa9e5
DongIkkk <w8h0412@gmail.com>
2022-09-28 10:27:35 +0900

    day3 test

---
+++ customuser/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import CustomModel
+
+admin.site.register(CustomModel)
---
+++ customuser/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomModel(AbstractUser):
+    class Meta:
+        db_table = "custom_user"
+
+    address = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
